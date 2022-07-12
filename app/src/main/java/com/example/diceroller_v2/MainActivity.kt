4baa2f4bca8d12ec351ad7f13d1c3a146146eb68
LowJiaHie <jiahielow8@gmail.com>
2022-07-12 17:32:20 +0800

    update 12/7/2022 Prac

--- app/src/main/java/com/example/diceroller_v2/MainActivity.kt
+++ app/src/main/java/com/example/diceroller_v2/MainActivity.kt
@@ -5,36 +5,43 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     lateinit var diceImage: ImageView
+    lateinit var numText : TextView
 //    lateinit var diceImage2: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
-        val clearButton: Button = findViewById(R.id.clear_button)
+        val clearButton = findViewById<Button>(R.id.clear_button)
         rollButton.setOnClickListener { rollDice() }
         clearButton.setOnClickListener { clear() }
         diceImage = findViewById(R.id.dice_image)
+        numText = findViewById(R.id.num_text)
 //        diceImage2 = findViewById(R.id.dice_image2)
     }
 
     private fun rollDice() {
-        diceImage.setImageResource(getRandomDiceImage())
+        val (randomImage, randomNum) = getRandomDiceImage()
+        numText.text = randomNum.toString()
+        diceImage.setImageResource(randomImage)
 //        diceImage2.setImageResource(getRandomDiceImage())
+        Toast.makeText(this, randomNum.toString(),Toast.LENGTH_SHORT).show()
     }
 
     //challenge coding
-    private fun getRandomDiceImage(): Int{
-        return when((1..6).random()){
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
+    private fun getRandomDiceImage(): Pair<Int,Int>{
+        val randomNum = (1..6).random()
+        return when(randomNum){
+            1 -> Pair(R.drawable.dice_1,randomNum)
+            2 -> Pair(R.drawable.dice_2,randomNum)
+            3 -> Pair(R.drawable.dice_3,randomNum)
+            4 -> Pair(R.drawable.dice_4,randomNum)
+            5 -> Pair(R.drawable.dice_5,randomNum)
+            else -> Pair(R.drawable.dice_6,randomNum)
         }
     }
 
